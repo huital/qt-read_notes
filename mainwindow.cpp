780d0339c779d1b2cfa9cea6124d6c2d62ac1d06
huital <1163204999@qq.com>
2023-10-17 17:36:01 +0800

    aa

---
+++ mainwindow.cpp
@@ -0,0 +1,284 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::init_tree_view(QString name, bool flag)
+{
+    model = new QStandardItemModel(this);
+    model->setHorizontalHeaderLabels(QStringList() << name);
+    // 如果是以打开已有笔记集的方式，那么需要读取下面所有文件
+    if (!flag) {
+       dir_real_all(noteFile, 0);
+    }
+     ui->tView_note->setModel(model);
+     ui->tView_note->setEditTriggers(QTreeView::NoEditTriggers);
+    // 创建右键菜单功能
+    ui->tView_note->setContextMenuPolicy(Qt::CustomContextMenu);
+    connect(ui->tView_note, &QTreeView::customContextMenuRequested, this, &MainWindow::slotTreeMenu);
+    codec=QTextCodec::codecForName("utf-8");
+    QTextCodec::setCodecForLocale(codec);
+}
+
+QStringList MainWindow::dir_real_all(const QString &dirPath, int cur_index)
+{
+    QStringList fileList;
+    QDir dir(dirPath);
+    QFileInfoList fileInfoList = dir.entryInfoList(QDir::Files | QDir::NoDotAndDotDot | QDir::Dirs);
+    // 递归读取
+    foreach (auto fileInfo, fileInfoList)
+    {
+        QString dir_name = fileInfo.absoluteFilePath().split('/').last();  // 从绝对路径中获取文件夹名称
+        // 判断是否为文件夹类型，且名称如果是answers跳过
+        if(fileInfo.isDir() && dir_name != "answers")
+        {
+            // 判断选择的是几级目录，这里简化写法
+
+            if (cur_index == 0) {   // 一级目录，大章节
+                items_1 = new QStandardItem(dir_name);
+                model->appendRow(items_1);
+            } else if (cur_index == 1){    // 二级目录，表示子章节
+                items_2 = new QStandardItem(dir_name);
+                items_1->appendRow(items_2);
+            }
+            // 继续向内搜索
+            dir_real_all(fileInfo.absoluteFilePath(), cur_index+1);
+        }
+
+        if (fileInfo.isFile())
+        {
+            items_3 = new QStandardItem(dir_name);
+            items_2->appendRow(items_3);
+        }
+
+    }
+    return fileList;
+}
+
+QString MainWindow::getFile(QModelIndex curIndex)
+{
+    QStack<QString> names;
+    QString filePath(noteFile);
+    // 依次将名称添加
+    while(curIndex.isValid()) {
+        names.push(curIndex.data().toString());
+        curIndex = curIndex.parent();
+    }
+    while(!names.isEmpty()) {
+        filePath.append('/'+names.top());
+        names.pop();
+    }
+    return filePath;
+}
+
+void MainWindow::create_note(QFile *file)
+{
+    if (!file->open(QIODevice::Append | QIODevice::Text))
+    {
+        QMessageBox::critical(this,"错误","文件打开失败，信息没有保存！","确定");
+        return;
+    }
+    file->close();
+}
+
+void MainWindow::save_file(QFile *fileProblem, QFile *fileAnswer)
+{
+    if (fileProblem->open(QIODevice::WriteOnly | QIODevice::Text) && fileAnswer->open(QIODevice::WriteOnly | QIODevice::Text))
+    {
+        QTextStream out_problem(fileProblem);
+        QTextStream out_answer(fileAnswer);
+        QString problem_content = ui->txtProblem->toPlainText();
+        QString answer_content = ui->txtAnswer->toPlainText();
+        out_problem << problem_content;
+        out_answer << answer_content;
+        fileProblem->close();
+        fileAnswer->close();
+        QMessageBox::information(this, "笔记保存对话框", "笔记修改成功", QMessageBox::Ok, QMessageBox::Ok);
+    }
+}
+
+QString MainWindow::read_note(QFile *file)
+{
+    QString content;
+    if (file->open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        QTextStream in(file);
+        content = in.readAll();
+    }
+    file->close();
+    return content;
+}
+
+// 用于创建笔记集（确定路径）
+void MainWindow::on_action_triggered()
+{
+    ui->txtAnswer->clear();
+    ui->txtProblem->clear();
+//    qDebug() << "this is triggered!";
+    QFileDialog *fileDialog = new QFileDialog(this);
+    fileDialog->setFileMode(QFileDialog::Directory);    // 设置后可选择目录
+    fileDialog->setDirectory("E:/Users");
+    fileDialog->setViewMode(QFileDialog::Detail);
+    // 选择创建路径并设置名称
+    if (fileDialog->exec()) {
+        noteFile = fileDialog->selectedFiles()[0];  // 获得笔记的基路径
+        bool ok = false;
+        noteName = QInputDialog::getText(this, "笔记集创建", "请输入笔记集名称", QLineEdit::Normal, "", &ok);
+        if (ok && !noteName.isEmpty()) {
+            qDebug() << "当前路径：" << noteFile << endl;
+            QDir dir(noteFile);
+            // 判断是否重名
+            if (!dir.exists(noteName))
+            {
+                dir.mkdir(noteName);
+
+                QMessageBox::information(this, "笔记集创建对话框", "笔记集创建成功", QMessageBox::Ok, QMessageBox::Ok);
+                init_tree_view(noteName, true);
+            } else {
+                QMessageBox::critical(this, "笔记集创建对话框", "笔记集创建失败", QMessageBox::Ok, QMessageBox::Ok);
+            }
+        }
+
+    }
+
+}
+
+void MainWindow::slotTreeMenu(const QPoint &pos)
+{
+    QMenu menu;
+
+    QModelIndex curIndex = ui->tView_note->indexAt(pos);    // 获取当前选中的行索引
+    if (curIndex.isValid()) {
+        // 不同层级的item需要创建不同右键菜单
+        if (curIndex.parent() == ui->tView_note->rootIndex()) {
+            QAction* actionParent = menu.addAction(QStringLiteral("添加子章节"), this, &MainWindow::slotAdditem);
+        }
+        if (curIndex.parent().isValid() && curIndex.parent().parent() == ui->tView_note->rootIndex())
+        {
+            QAction* actionProblems = menu.addAction(QStringLiteral("添加新问题"), this, &MainWindow::slotAddNote);
+        }
+    }
+
+    menu.exec(QCursor::pos());
+
+}
+
+// 在菜单设置处被链接
+void MainWindow::slotAdditem()
+{
+
+    QModelIndex curIndex = ui->tView_note->currentIndex();
+    QString parentName = curIndex.data().toString();
+    QString chapterFile = noteFile + "/" + parentName;
+    qDebug() << "当前路径：" << chapterFile << endl;
+    bool ok = false;
+    QDir dir(chapterFile);
+    QString chapterName = QInputDialog::getText(this, "子章节创建", "请输入子章节名称", QLineEdit::Normal, "", &ok);
+    if(ok && !dir.exists(chapterName)) {
+        dir.mkdir(chapterName);
+        QDir sub_dir(chapterFile+'/'+chapterName);
+        sub_dir.mkdir("problems");
+        sub_dir.mkdir("answers");
+        QMessageBox::information(this, "子章节创建对话框", "子章节创建成功", QMessageBox::Ok, QMessageBox::Ok);
+        items_2 = new QStandardItem(chapterName);
+        items_1 = model->itemFromIndex(curIndex);
+        items_1->appendRow(items_2);
+    } else {
+        QMessageBox::critical(this, "子章节创建对话框", "子章节创建失败", QMessageBox::Ok, QMessageBox::Ok);
+    }
+}
+// 在菜单设置处被链接
+void MainWindow::slotAddNote()
+{
+    QModelIndex curIndex = ui->tView_note->currentIndex();  // 获取当前选中行
+    // 生成路径
+    QString chapterFile = getFile(curIndex);
+    bool ok = false;
+    QDir dir(chapterFile + '/' + dir1_name);
+    QString chapterName = QInputDialog::getText(this, "笔记问题创建", "请输入笔记问题名称", QLineEdit::Normal, "", &ok);
+    if(ok && !dir.exists(chapterName)) {
+        problem_file = new QFile(chapterFile+'/'+dir1_name+'/'+chapterName+".txt");
+        answer_file = new QFile(chapterFile+'/'+dir2_name+'/'+chapterName+".txt");
+        qDebug() << "文件名称" << chapterFile+'/'+dir2_name+'/'+chapterName+".txt" << endl;
+        create_note(problem_file);
+        create_note(answer_file);
+        QMessageBox::information(this, "笔记问题创建对话框", "笔记问题创建成功", QMessageBox::Ok, QMessageBox::Ok);
+        items_2 = new QStandardItem(chapterName+".txt");
+        items_1 = model->itemFromIndex(curIndex);
+        items_1->appendRow(items_2);
+    } else {
+        QMessageBox::critical(this, "笔记问题创建对话框", "笔记问题创建失败", QMessageBox::Ok, QMessageBox::Ok);
+    }
+}
+
+void MainWindow::on_action_2_triggered()
+{
+    ui->txtAnswer->clear();
+    ui->txtProblem->clear();
+    QFileDialog *fileDialog = new QFileDialog(this);
+    fileDialog->setFileMode(QFileDialog::Directory);
+    fileDialog->setDirectory("E:/Users");
+    fileDialog->setViewMode(QFileDialog::Detail);
+    if (fileDialog->exec()) {
+        noteFile = fileDialog->selectedFiles()[0];
+        noteName = noteFile.split('/').last();
+        init_tree_view(noteName, false);
+    }
+
+
+}
+
+void MainWindow::on_actCreateChapter_triggered()
+{
+
+}
+
+void MainWindow::on_action_4_triggered()
+{
+    bool ok = false;
+    QDir dir(noteFile);
+    QString chapterName = QInputDialog::getText(this, "章节创建", "请输入新章节名称", QLineEdit::Normal, "", &ok);
+    if(ok && !dir.exists(chapterName)) {
+        dir.mkdir(chapterName);
+        QMessageBox::information(this, "章节创建对话框", "章节创建成功", QMessageBox::Ok, QMessageBox::Ok);
+        items_1 = new QStandardItem(chapterName);
+        model->appendRow(items_1);
+    } else {
+        QMessageBox::critical(this, "章节创建对话框", "章节创建失败", QMessageBox::Ok, QMessageBox::Ok);
+    }
+}
+
+void MainWindow::on_tView_note_doubleClicked(const QModelIndex &index)
+{
+    QString fileName = index.data().toString();
+    if(fileName.endsWith(".txt")) {
+        QString chapterFile = getFile(index.parent());
+        problem_file = new QFile(chapterFile+'/'+dir1_name+'/'+fileName);
+        answer_file = new QFile(chapterFile+'/'+dir2_name+'/'+fileName);
+        qDebug() << "current answerName:" << chapterFile+'/'+dir2_name+'/'+fileName << endl;
+        ui->txtProblem->setText(read_note(problem_file));
+        ui->txtAnswer->clear();
+
+    }
+}
+
+void MainWindow::on_pButton_show_clicked()
+{
+    ui->txtAnswer->setText(read_note(answer_file));
+
+}
+
+void MainWindow::on_pBtn_save_clicked()
+{
+    save_file(problem_file, answer_file);
+}
---
+++ mainwindow.h
@@ -0,0 +1,70 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QDebug>
+#include <QFileDialog>
+#include <QInputDialog>
+#include <QMessageBox>
+#include <QDir>
+#include <QStandardItemModel>
+#include <QPoint>
+#include <QMenu>
+#include <QStack>
+#include <QFile>
+#include <QTextCodec>
+namespace Ui {
+class MainWindow;
+}
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MainWindow(QWidget *parent = 0);
+    ~MainWindow();
+    void init_tree_view(QString name, bool flag);       // 初始化文件列表
+    QStringList dir_real_all(const QString &dirPath, int cur_index);    // 读取一个目录下的所有文件内容，并以treeview展示
+    QString getFile(QModelIndex curIndexe);     // 根据当前的选中行，生成其绝对路径
+    void create_note(QFile *file);              // 文件创建
+    void save_file(QFile *fileProblem, QFile *fileAnswer);      // 文件保存
+    QString read_note(QFile *file);             // 文件读取
+
+private slots:
+    void on_action_triggered();             // 创建笔记
+    void slotTreeMenu(const QPoint &pos);       // 设置右键菜单
+    void slotAdditem();             // 添加子目录
+    void slotAddNote(); // 添加问题
+    void on_action_2_triggered();
+
+    void on_actCreateChapter_triggered();
+
+    void on_action_4_triggered();       // 创建大章节
+
+    void on_tView_note_doubleClicked(const QModelIndex &index); // 双击文件时，显示问题内容
+
+    void on_pButton_show_clicked();     // 显示答案内容
+
+    void on_pBtn_save_clicked();        // 保存问题和答案的内容
+
+private:
+    Ui::MainWindow *ui;
+    QString noteFile;   // 笔记集路径（一级）
+    QString noteName;   // 笔记集名称
+    QString chapterName;    // 章节名称
+    QString subchapterName; // 子章节名称
+    QString questionName;      // 问题名称
+    QStandardItemModel *model;  // treeview的数据模型
+    QStandardItem* items_1;  // 一级索引
+    QStandardItem* items_2;  // 二级索引
+    QStandardItem* items_3;  // 三级索引
+    QString dir1_name = "problems";
+    QString dir2_name = "answers";
+    QFile* problem_file;    // 问题文件
+    QFile* answer_file;     // 答案文件
+    QTextCodec *codec;      // 编码格式，防止中文乱码
+
+};
+
+#endif // MAINWINDOW_H
